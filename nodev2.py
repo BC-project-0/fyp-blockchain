8f5d7d54a1a90695f21a7079315115bd7cd73842
madraceee <raajanitheesh@gmail.com>
2024-03-13 20:31:50 +0530

    Changes to registration and auth

--- nodev2.py
+++ nodev2.py
@@ -9,19 +9,25 @@ import os
 from BullyNode import BullyNode
 from BullyAlgo import *
 from Blockchain import Blockchain
+from starlette.responses import FileResponse
+from utils import verify_initial_signature
+from utils import verify_signature
 
 
 class RegisterPayload(BaseModel):
     id: str
     commitment_value: str
     public_key: str
+    signature: str
 
 
 class AuthenticationPayload(BaseModel):
     id: str
-    commitment_value: str
+    old_commitment_value: str
+    new_commitment_value: str
     signature: str
     index: int
+    pk: str
 
 
 app = FastAPI()
@@ -77,32 +83,42 @@ async def registration(body: RegisterPayload):
     unique_id = body.id
     commitment_value = body.commitment_value
     public_key = body.public_key
+    signature = body.signature
     if unique_id in blockchain.unique_id_to_commitment_value_mapping:
         return {"message": "User Already Exists"}
     else:
-        blockchain.unique_id_to_commitment_value_mapping[unique_id] = {
-            "index": 0, "commitment_value": commitment_value, "public_key": public_key}
-        event, data = "Registration", "{}:{}:{}".format(
-            unique_id, commitment_value, public_key)
-        node.send_encrypted_msg(event, data)
-        return {"message": "User Registered"}
+        if(verify_initial_signature(public_key,unique_id,commitment_value,signature)):
+            blockchain.unique_id_to_commitment_value_mapping[unique_id] = {
+                "index": 1, "commitment_value": commitment_value, "public_key": public_key}
+            event, data = "Registration", "{}:{}:{}".format(
+                unique_id, commitment_value, public_key)
+            node.send_encrypted_msg(event, data)
+            return {"message": "User Registered"}
+        else:
+            return {"message": "User Could not be registered"}
 
+
+@app.get("/authenticate")
+async def authenticateFrontend():
+    return FileResponse("authenticate.html")
 
 @app.post("/authenticate")
 async def authentication(body: AuthenticationPayload):
     unique_id = body.id
-    commitment_value = body.commitment_value
+    old_commitment_value = body.old_commitment_value
+    new_commitment_value = body.new_commitment_value
     signature = body.signature
     index = body.index
+    pk = body.pk
+    if verify_signature(pk.encode("utf-8"),unique_id,old_commitment_value,new_commitment_value,index,signature) == False: 
+        return {"message": "User Authentication Failed"}
+
     if unique_id in blockchain.unique_id_to_commitment_value_mapping:
-        record = blockchain.unique_id_to_commitment_value_mapping[unique_id]
-        previous_commitment_value = record["commitment_value"]
-        index = record["index"]
         result = Blockchain.authenticate(
-            blockchain, unique_id, previous_commitment_value, commitment_value, index, signature)
+            blockchain, unique_id, old_commitment_value, new_commitment_value, index, signature)
         if result == index + 1:
             event, msg = "Record Update", "{}:{}:{}".format(
-                unique_id, result, commitment_value)
+                unique_id, result, new_commitment_value)
             node.send_encrypted_msg(event, msg)
             return {"message": "User Authentication Success", "index": result}
         else:
--- utils.py
+++ utils.py
@@ -2,12 +2,18 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
+import base64
 
+def decodeBase64(base64_string):
+    base64_bytes = base64_string.encode("latin-1")
+    return base64.b64decode(base64_bytes)
 
-def verify_initial_signature(public_key, id, y0, signature):
+def verify_initial_signature(pubKey, id, y0, signature):
+    
+    signature = decodeBase64(signature)
     # Load the public key
     public_key = serialization.load_pem_public_key(
-        public_key,
+        pubKey.encode("utf-8"),
         backend=default_backend()
     )
 
@@ -20,7 +26,7 @@ def verify_initial_signature(public_key, id, y0, signature):
             concatenated_str.encode(),
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
+                salt_length=20
             ),
             hashes.SHA256()
         )
@@ -33,6 +39,8 @@ def verify_initial_signature(public_key, id, y0, signature):
 
 
 def verify_signature(public_key, id, xi, yi, i, signature):
+    
+    signature = decodeBase64(signature)
     # Load the public key
     public_key = serialization.load_pem_public_key(
         public_key,
@@ -48,7 +56,7 @@ def verify_signature(public_key, id, xi, yi, i, signature):
             concatenated_str.encode(),
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
+                salt_length=20
             ),
             hashes.SHA256()
         )
